bc5c030cc78f224fbc639eb55357d5a266657083
Hamdani Ilham <d41113512@gmail.com>
2024-05-01 19:12:45 +0800

    data bar

--- financial-bar.py
+++ financial-bar.py
@@ -1,23 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 from datetime import datetime
 
 df = pd.read_csv('data/HistoricalData.csv')
 
 df['Average'] = df['High'].str.replace('$', '').astype(float) + df['Low'].str.replace('$', '').astype(float) / 2
 
-df['Date'] = pd.to_datetime(df['Date'], format="%m/%d/%Y")
-
+# df['Date'] = pd.to_datetime(df['Date'], format="%m/%d/%Y")
 df_sorted = df.sort_values(by='Date', ascending=False)
-# df_sorted['newDate'] = df_sorted["Date"].apply(lambda x: x.strftime("%d %B %Y")) 
-print(df_sorted)
 
-fig, axs = plt.subplots(1, 1, figsize=(15, 5))
+fig, ax = plt.subplots(1, 1, figsize=(15, 5))
 
-axs.plot(df_sorted['Date'], df_sorted['Average'])
-axs.set_ylabel('Avarege')
-axs.set_xlabel('Date')
+ax.bar(df_sorted['Date'], df_sorted['Average'])
+
+ax.set_ylabel('Avarege')
+ax.set_xlabel('Date')
 
 fig.suptitle('FINANCIAL DATA ANALYSIS')
 fig.autofmt_xdate()
+
 plt.show()
